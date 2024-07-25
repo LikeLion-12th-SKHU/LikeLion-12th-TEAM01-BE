4408a84e195bdc82a16530f828ec2f99ccbc2178
Lee, yeeun <ye7426@naver.com>
2024-07-25 22:36:04 +0900

    :sparkles:Feat: CommentInfoResponseDTO 추가

---
+++ src/main/java/org/skhuton/fitpete/community/comment/api/dto/response/CommentInfoResponseDTO.java
@@ -0,0 +1,19 @@
+package org.skhuton.fitpete.community.comment.api.dto.response;
+
+import lombok.Builder;
+import org.skhuton.fitpete.community.comment.domain.Comment;
+
+@Builder
+public record CommentInfoResponseDTO(
+        Long writerMemberId,
+        Long commentId,
+        String content
+) {
+    public static CommentInfoResponseDTO of(Comment comment) {
+        return CommentInfoResponseDTO.builder()
+                .writerMemberId(comment.getWriter().getMemberId())
+                .commentId(comment.getCommentId())
+                .content(comment.getContent())
+                .build();
+    }
+}
