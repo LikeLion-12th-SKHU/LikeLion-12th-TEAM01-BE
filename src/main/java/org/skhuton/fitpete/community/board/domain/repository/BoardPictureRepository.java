ff67bad19743652434d5c14aeac26f5999bec97b
Lee, yeeun <ye7426@naver.com>
2024-07-25 22:31:21 +0900

    :sparkles:Feat: BoardPictureRepository 추가

---
+++ src/main/java/org/skhuton/fitpete/community/board/domain/repository/BoardPictureRepository.java
@@ -0,0 +1,13 @@
+package org.skhuton.fitpete.community.board.domain.repository;
+
+import org.skhuton.fitpete.community.board.domain.Board;
+import org.skhuton.fitpete.community.board.domain.BoardPicture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardPictureRepository extends JpaRepository<BoardPicture, Long> {
+    void deleteByBoardBoardId(Long boardId);
+
+    void deleteByBoardAndImageUrlIn(Board board, List<String> urlsToDelete);
+}
