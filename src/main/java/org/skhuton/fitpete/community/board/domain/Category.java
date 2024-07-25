ff9dd17ce6e90a178fb74b19d967675fc928ce95
Lee, yeeun <ye7426@naver.com>
2024-07-25 22:33:53 +0900

    :sparkles:Feat: Category 추가

---
+++ src/main/java/org/skhuton/fitpete/community/board/domain/Category.java
@@ -0,0 +1,20 @@
+package org.skhuton.fitpete.community.board.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Category {
+    DIET("다이어트"),
+    WATER("물"),
+    EXERCISE("운동"),
+    SUPPLEMENT("영양제"),
+    SLEEP("잠"),
+    SEX("성건강");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+
+}
