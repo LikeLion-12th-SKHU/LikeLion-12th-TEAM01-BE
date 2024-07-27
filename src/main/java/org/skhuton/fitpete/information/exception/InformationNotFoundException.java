18e0a70092aeac1ff6881236e3a728681eed73a5
ckr8305 <ckr8305@naver.com>
2024-07-27 17:23:46 +0900

    :sparkles:Feat: InformationNotFoundException 추가

--- src/main/java/org/skhuton/fitpete/information/exception/InformationNotFoundException.java
+++ src/main/java/org/skhuton/fitpete/information/exception/InformationNotFoundException.java
@@ -4,4 +4,5 @@ import org.skhuton.fitpete.auth.global.error.exception.NotFoundException;
 
 public class InformationNotFoundException extends NotFoundException {
     public InformationNotFoundException(String message) { super("Information not found"); }
+
 }
