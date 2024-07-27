9c006d6e6be000e0a7846d8af2edcc144dab2e7f
ckr8305 <ckr8305@naver.com>
2024-07-27 15:40:32 +0900

    :sparkles:Feat: ExistsInformationRecommendException 추가

---
+++ src/main/java/org/skhuton/fitpete/information/exception/ExistsInformationRecommendException.java
@@ -0,0 +1,9 @@
+package org.skhuton.fitpete.information.exception;
+
+import org.skhuton.fitpete.auth.global.error.exception.InvalidException;
+
+public class ExistsInformationRecommendException extends InvalidException {
+    public ExistsInformationRecommendException(String message) {
+        super(message);
+    }
+}
