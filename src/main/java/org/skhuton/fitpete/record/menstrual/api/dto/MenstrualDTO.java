c9ea3748d22c7fdbd3035fc573bd1f63ef96bd36
Lee, yeeun <ye7426@naver.com>
2024-08-03 23:51:05 +0900

    :recycle:Refactor: MenstrualDTO 기능 추가

--- src/main/java/org/skhuton/fitpete/record/menstrual/api/dto/MenstrualDTO.java
+++ src/main/java/org/skhuton/fitpete/record/menstrual/api/dto/MenstrualDTO.java
@@ -1,11 +1,11 @@
 package org.skhuton.fitpete.record.menstrual.api.dto;
 
 import lombok.Builder;
-
 import java.time.LocalDate;
 
 @Builder
 public record MenstrualDTO(
+        Long menstrualId,
         Boolean menstrualCycle,
         LocalDate menstrualStartDate,
         LocalDate menstrualEndDate
