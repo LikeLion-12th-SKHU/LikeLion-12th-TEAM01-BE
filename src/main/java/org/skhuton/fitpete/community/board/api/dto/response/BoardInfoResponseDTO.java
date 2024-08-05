eee83a9e00d185fb9f900cdcc3cb2f2f0068f962
Lee, yeeun <ye7426@naver.com>
2024-08-05 14:42:13 +0900

    :recycle:Refactor: BoardInfoResponseDTO 수정

--- src/main/java/org/skhuton/fitpete/community/board/api/dto/response/BoardInfoResponseDTO.java
+++ src/main/java/org/skhuton/fitpete/community/board/api/dto/response/BoardInfoResponseDTO.java
@@ -2,10 +2,8 @@ package org.skhuton.fitpete.community.board.api.dto.response;
 
 import lombok.Builder;
 import org.skhuton.fitpete.community.board.domain.Board;
-import org.skhuton.fitpete.community.board.domain.BoardPicture;
 import org.skhuton.fitpete.community.board.domain.Category;
 import org.skhuton.fitpete.community.comment.api.dto.response.CommentInfoResponseDTO;
-import org.skhuton.fitpete.community.comment.domain.Comment;
 import org.skhuton.fitpete.member.domain.Member;
 
 
@@ -28,9 +26,9 @@ public record BoardInfoResponseDTO(
         List<CommentInfoResponseDTO> comments
 ) {
     public static BoardInfoResponseDTO of (Member member, Board board) {
-        List<String> imageUrl = board.getPictures().stream()
-                .map(BoardPicture::getImageUrl)
-                .toList();
+//        List<String> imageUrl = board.getPictures().stream()
+//                .map(BoardPicture::getImageUrl)
+//                .toList();
 
         return BoardInfoResponseDTO.builder()
                 .myMemberId(member.getMemberId())
@@ -39,7 +37,7 @@ public record BoardInfoResponseDTO(
                 .category(board.getCategory())
                 .title(board.getTitle())
                 .content(board.getContent())
-                .imageUrl(imageUrl)
+//                .imageUrl(imageUrl)
                 .likeCount(board.getRecommendCount())
                 .commentCount(board.getComments().size())
                 .nickname(board.getWriter().getNickname())
@@ -53,9 +51,9 @@ public record BoardInfoResponseDTO(
 
 
     public static BoardInfoResponseDTO detailOf(Member member, Board board, boolean isLike) {
-        List<String> imageUrl = board.getPictures().stream()
-                .map(BoardPicture::getImageUrl)
-                .toList();
+//        List<String> imageUrl = board.getPictures().stream()
+//                .map(BoardPicture::getImageUrl)
+//                .toList();
 
         List<CommentInfoResponseDTO> commentInfoResponseDTO = board.getComments().stream()
                 .map(CommentInfoResponseDTO::of)
@@ -68,7 +66,7 @@ public record BoardInfoResponseDTO(
                 .category(board.getCategory())
                 .title(board.getTitle())
                 .content(board.getContent())
-                .imageUrl(imageUrl)
+//                .imageUrl(imageUrl)
                 .likeCount(board.getRecommendCount())
                 .isLike(isLike)
                 .commentCount(commentInfoResponseDTO.size())
